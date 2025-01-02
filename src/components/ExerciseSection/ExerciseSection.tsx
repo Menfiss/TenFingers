16d91f21b668e879320023f6d004fa875dfc563e
Lukas Karsky <menfiscz123@gmail.com>
2025-01-02 19:36:49 +0100

    dropdown working

--- src/components/ExerciseSection/ExerciseSection.tsx
+++ src/components/ExerciseSection/ExerciseSection.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState } from "react";
 import ExerciseButton from "../ExerciseButton/ExerciseButton";
 
 interface props{
@@ -23,6 +26,7 @@ interface props{
 }
 
 const ExerciseSection = (props:props) => {
+    const [loadExercises, setLoadExercises] = useState(false);
     let unlocked = props.firstExerciseUnlocked; // should the first exercise be unlocked
 
     function checkIfUnlocked(stars:number){
@@ -83,13 +87,16 @@ const ExerciseSection = (props:props) => {
     
     return (
         <div>
-            <div className="flex mb-4 mt-16">
-                <h2 className="text-2xl">{props.section.name}</h2>
-                <h2 className="text-xl leading-8 ml-4">{" " + calculateStars() + "/"+ props.section.exercises.length*3}</h2>
-                <svg className="w-4 h-4 mt-2 text-yellow-300 ms-1" aria-hidden="true" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
-            </div>
+            <button onClick={() => setLoadExercises(!loadExercises)} className="w-full">
+                <div className="flex mb-4 mt-16">
+                    <h2 className="text-2xl">{props.section.name}</h2>
+                    <h2 className="text-xl leading-8 ml-4">{" " + calculateStars() + "/"+ props.section.exercises.length*3}</h2>
+                    <svg className="w-4 h-4 mt-2 text-yellow-300 ms-1" aria-hidden="true" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                </div>
+            </button>
+            {loadExercises &&
             <div className="grid grid-cols-2 gap-6 lg:grid-cols-4">
               {orderedExercises.map((exercise_index, index) => {
                 let stars = 0;
@@ -111,7 +118,7 @@ const ExerciseSection = (props:props) => {
                      <ExerciseButton key={index} unlocked={checkIfUnlocked(stars)} stars={stars} wpm={wpm} accuracy={accuracy} time={time} id={props.section.exercises[exercise_index].id}></ExerciseButton>
                 )
                 })}
-            </div>
+            </div>}
         </div>
     )
 }
